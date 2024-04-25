724af6702840c6ce98a40a5b9cd82930bc41b660
Omaribrahim43 <omar.migdadi43@gmail.com>
2024-04-26 02:05:08 +0300

    prediction steps saveing input values changes

--- src/jsx/components/Forms/Wizard/StepFour.js
+++ src/jsx/components/Forms/Wizard/StepFour.js
@@ -1,14 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-const StepFour = () => {
-   return (
-      <section>
-         <div className="text-center">
-         <button type="button" class="me-2 btn btn-primary btn-rounded">Predict</button>
-         </div>
-         {/* Add a loading thingie */}
-         {/* <div className="row">
+const StepFour = ({ formData }) => {
+  const handleSubmit = () => {
+    // Handle form submission using formData
+    console.log("Form Data:", formData);
+    // Perform necessary actions, such as sending data to server
+  };
+  return (
+    <section>
+      <div>
+        {formData.name === "" && formData.age === "" ? (
+          ""
+        ) : (
+          <p className="text-center">
+            You are predicting stroke probability for the patient{" "}
+            {formData.name} at the age {formData.age}
+          </p>
+        )}
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <input type="hidden" name="name" value={formData.name || ""} />
+        <input type="hidden" name="age" value={formData.age || ""} />
+        <input type="hidden" name="gender" value={formData.gender || ""} />
+        <input type="hidden" name="name" value={formData.race || ""} />
+        <input
+          type="hidden"
+          name="treatment"
+          value={formData.treatment || ""}
+        />
+        <input
+          type="hidden"
+          name="yearOfFollowUpRecord"
+          value={formData.yearOfFollowUpRecord || ""}
+        />
+        <input type="hidden" name="cancer" value={formData.cancer || ""} />
+        {/* Add other form fields here based on your previous steps */}
+        <div className="text-center">
+          <button type="submit" className="me-2 btn btn-primary btn-rounded">
+            Predict
+          </button>
+        </div>
+      </form>
+      {/* Add a loading thingie */}
+      {/* <div className="row">
             <div className="col-12">
                <div className="skip-email text-center pt-5 pb-2">
                   <p>Or if want skip this step entirely and setup it later</p>
@@ -18,8 +54,8 @@ const StepFour = () => {
                </div>
             </div>
          </div> */}
-      </section>
-   );
+    </section>
+  );
 };
 
 export default StepFour;
--- src/jsx/components/Forms/Wizard/StepOne.js
+++ src/jsx/components/Forms/Wizard/StepOne.js
@@ -1,67 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
 import Select from "react-select";
-import selectOptions from '../../../../jsons/selectOptions.json';
+import selectOptions from "../../../../jsons/selectOptions.json";
 
+const StepOne = ({ formData, setFormData }) => {
+  const [name, setName] = useState(formData.name || "");
+  const [age, setAge] = useState(formData.age || "");
+  const [gender, setGender] = useState(
+    formData.gender || selectOptions.genderOptions[0]
+  );
+  const [race, setRace] = useState(
+    formData.race || selectOptions.raceOptions[0]
+  );
 
-const StepOne = () => {
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+    setFormData({ ...formData, name: e.target.value });
+  };
 
+  const handleAgeChange = (e) => {
+    setAge(e.target.value);
+    setFormData({ ...formData, age: e.target.value });
+  };
 
-   return (
-      <section>
-         <div className="row">
-            <div className="col-lg-6 mb-2">
-               <div className="form-group mb-3">
-                  <label className="text-label">Name <span className="required">*</span></label>
-                  <input
-                     type="text"
-                     name="name"
-                     className="form-control"
-                     placeholder=""
-                     required
-                  />
-               </div>
-            </div>
-            <div className="col-lg-6 mb-2">
-               <div className="form-group mb-3">
-                  <label className="text-label">age<span className="required">*</span></label>
-                  <input
-                     name="age"
-                     type="number"
-                     className="form-control"
-                     id="inputGroupPrepend2"
-                     aria-describedby="inputGroupPrepend2"
-                     required
-                  />
-               </div>
-            </div>
-            <div className="col-lg-6 mb-2">
-               <div className="form-group mb-3">
-               <label className="form-label">Gender</label>
-                    <Select
-                      name="gender"
-                      options={selectOptions.genderOptions}
-                      className="custom-react-select"
-                      defaultValue={selectOptions.genderOptions[0]}
-                      isSearchable={false}
-                    />
-               </div>
-            </div>
-            <div className="col-lg-6 mb-3">
-               <div className="form-group mb-3">
-               <label className="form-label">Race</label>
-               <Select
-                      name="race"
-                      options={selectOptions.raceOptions}
-                      className="custom-react-select"
-                      defaultValue={selectOptions.raceOptions[0]}
-                      isSearchable={false}
-                      id="racerace"
-                    />
-               </div>
-            </div>
-         </div>
-      </section>
-   );
+  const handleGenderChange = (selectedOption) => {
+    setGender(selectedOption);
+    setFormData({ ...formData, gender: selectedOption });
+  };
+
+  const handleRaceChange = (selectedOption) => {
+    setRace(selectedOption);
+    setFormData({ ...formData, race: selectedOption });
+  };
+
+  return (
+    <section>
+      <div className="row">
+        <div className="col-lg-6 mb-2">
+          <div className="form-group mb-3">
+            <label className="text-label">
+              Name <span className="required">*</span>
+            </label>
+            <input
+              type="text"
+              name="name"
+              className="form-control"
+              placeholder=""
+              value={name}
+              onChange={handleNameChange}
+              required
+            />
+          </div>
+        </div>
+        <div className="col-lg-6 mb-2">
+          <div className="form-group mb-3">
+            <label className="text-label">
+              age<span className="required">*</span>
+            </label>
+            <input
+              name="age"
+              type="number"
+              className="form-control"
+              value={age}
+              onChange={handleAgeChange}
+              required
+            />
+          </div>
+        </div>
+        <div className="col-lg-6 mb-2">
+          <div className="form-group mb-3">
+            <label className="form-label">Gender</label>
+            <Select
+              name="gender"
+              options={selectOptions.genderOptions}
+              className="custom-react-select"
+              defaultValue={selectOptions.genderOptions[0]}
+              isSearchable={false}
+              value={gender}
+              onChange={handleGenderChange}
+            />
+          </div>
+        </div>
+        <div className="col-lg-6 mb-3">
+          <div className="form-group mb-3">
+            <label className="form-label">Race</label>
+            <Select
+              name="race"
+              options={selectOptions.raceOptions}
+              className="custom-react-select"
+              defaultValue={selectOptions.raceOptions[0]}
+              isSearchable={false}
+              value={race}
+              onChange={handleRaceChange}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default StepOne;
