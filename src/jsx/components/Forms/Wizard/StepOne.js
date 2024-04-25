0a2c837478836516627a96216b57dadc0aca1e2e
B1UG <noor.feraas@gmail.com>
2024-04-26 02:11:04 +0300

    noor

--- src/jsx/components/Forms/Wizard/StepOne.js
+++ src/jsx/components/Forms/Wizard/StepOne.js
@@ -37,68 +37,37 @@ const StepOne = ({ formData, setFormData }) => {
       <div className="row">
         <div className="col-lg-6 mb-2">
           <div className="form-group mb-3">
-<<<<<<< HEAD
-            <label className="text-label">Name <span className="required">*</span></label>
-=======
             <label className="text-label">
               Name <span className="required">*</span>
             </label>
->>>>>>> 724af6702840c6ce98a40a5b9cd82930bc41b660
             <input
               type="text"
               name="name"
               className="form-control"
               placeholder=""
-<<<<<<< HEAD
-              value={firstInfo.name}
-              onChange={handleChange}
-=======
               value={name}
               onChange={handleNameChange}
->>>>>>> 724af6702840c6ce98a40a5b9cd82930bc41b660
               required
             />
           </div>
         </div>
         <div className="col-lg-6 mb-2">
           <div className="form-group mb-3">
-<<<<<<< HEAD
-            <label className="text-label">Age<span className="required">*</span></label>
-=======
             <label className="text-label">
               age<span className="required">*</span>
             </label>
->>>>>>> 724af6702840c6ce98a40a5b9cd82930bc41b660
             <input
               name="age"
               type="number"
               className="form-control"
-<<<<<<< HEAD
-              id="inputGroupPrepend2"
-              aria-describedby="inputGroupPrepend2"
-              value={firstInfo.age}
-              onChange={handleChange}
-=======
               value={age}
               onChange={handleAgeChange}
->>>>>>> 724af6702840c6ce98a40a5b9cd82930bc41b660
               required
             />
           </div>
         </div>
         <div className="col-lg-6 mb-2">
           <div className="form-group mb-3">
-<<<<<<< HEAD
-            <label className="form-label">Gender<span className="required">*</span></label>
-            <Select
-              required
-              name="gender"
-              options={selectOptions.genderOptions}
-              className="custom-react-select"
-              value={firstInfo.gender}
-              onChange={(selectedOption) => setFirstInfo({...firstInfo, gender: selectedOption})}
-              isSearchable={false}
-=======
             <label className="form-label">Gender</label>
             <Select
               name="gender"
@@ -108,23 +77,11 @@ const StepOne = ({ formData, setFormData }) => {
               isSearchable={false}
               value={gender}
               onChange={handleGenderChange}
->>>>>>> 724af6702840c6ce98a40a5b9cd82930bc41b660
             />
           </div>
         </div>
         <div className="col-lg-6 mb-3">
           <div className="form-group mb-3">
-<<<<<<< HEAD
-            <label className="form-label">Race<span className="required">*</span></label>
-            <Select
-              required
-              name="race"
-              options={selectOptions.raceOptions}
-              className="custom-react-select"
-              value={"SDS"}
-              onChange={(selectedOption) => setFirstInfo({...firstInfo, race: selectedOption})}
-              isSearchable={false}
-=======
             <label className="form-label">Race</label>
             <Select
               name="race"
@@ -134,7 +91,6 @@ const StepOne = ({ formData, setFormData }) => {
               isSearchable={false}
               value={race}
               onChange={handleRaceChange}
->>>>>>> 724af6702840c6ce98a40a5b9cd82930bc41b660
             />
           </div>
         </div>
